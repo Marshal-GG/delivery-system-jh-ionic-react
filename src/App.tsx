897881c3483a8b1a7f78206fd66f1d55f432657b
Marshal <Marshalgcom@gmail.com>
2022-04-14 00:07:46 +0530

    Setting up route

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,15 @@ import {
   IonAlert,
   IonPage,
   IonSegment,
-  IonSegmentButton
+  IonSegmentButton,
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonRoute
 } from '@ionic/react';
 import { calculatorOutline, pin, refreshOutline } from 'ionicons/icons';
+import { Router } from 'react-router';
+import { Routes, Route, Link } from 'react-router-dom';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -46,6 +52,37 @@ setupIonicReact();
 
 const App: React.FC = () => {
 
+  function Home() {
+    return (
+      <>
+        <main>
+          <h2>Welcome to the homepage!</h2>
+          <p>You can do this, I believe in you.</p>
+        </main>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </>
+    );
+  }
+
+  function About() {
+    return (
+      <>
+        <main>
+          <h2>Who are we?</h2>
+          <p>
+            That feels like an existential question, don't you
+            think?
+          </p>
+        </main>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </>
+    );
+  }
+
   return (
     <React.Fragment>
       <IonApp>
@@ -57,8 +94,9 @@ const App: React.FC = () => {
               </IonTitle>
             </IonToolbar>
           </IonHeader>
-          <IonContent className='ion-padding'>
+          <IonContent fullscreen className='ion-padding'>
             <IonGrid>
+
               <IonRow>
                 <IonSegment value="First">
                   <IonSegmentButton value='First'>First</IonSegmentButton>
@@ -66,11 +104,13 @@ const App: React.FC = () => {
                   <IonSegmentButton value='Third'>Third</IonSegmentButton>
                 </IonSegment>
               </IonRow>
+
               <IonRow>
                 <IonCol>
                   Working
                 </IonCol>
               </IonRow>
+
             </IonGrid>
           </IonContent>
         </IonPage>
